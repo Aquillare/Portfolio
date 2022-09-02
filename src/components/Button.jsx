fd0d550a68a1029a9159d93b6acc1d5f2b240e03
Miguel <migueleduagui@gmail.com>
2022-09-02 17:30:33 -0300

    added identation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,10 +6,10 @@ const Button = ({element}) => {
 
     return(
         <a href={element.url} target="blank">
-        <button type="button" className="container_button">
-            <img src={element.src} alt={`${element.name}-icon`}/>
-            <span>{element.name}</span>
-        </button>
+            <button type="button" className="container_button">
+                <img src={element.src} alt={`${element.name}-icon`}/>
+                <span>{element.name}</span>
+            </button>
         </a>
     );
        
